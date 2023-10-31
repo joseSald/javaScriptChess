a16f4ed32dd0917b886793cab8e049a198d193a1
Pedro Guemes <saldunajose@gmail.com>
2023-10-31 06:17:41 -0300

    commit 2

---
+++ app.js
@@ -0,0 +1,82 @@
+const gameBoard = document.querySelector('#gameboard');
+const playerDisplay = document.querySelector('#player');
+const infoDisplay = document.querySelector('#info-display');
+const width = 8;
+const startPieces = [
+  rook,
+  knight,
+  bishop,
+  queen,
+  king,
+  bishop,
+  knight,
+  rook,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  rook,
+  knight,
+  bishop,
+  queen,
+  king,
+  bishop,
+  knight,
+  rook,
+];
+console.log(startPieces);
+function createBoard() {
+  startPieces.forEach((startPiece, i) => {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.innerHTML = startPiece;
+    square.setAttribute('square-id', i);
+    square.classList.add('beige');
+    gameBoard.append(square);
+  });
+}
+createBoard();
