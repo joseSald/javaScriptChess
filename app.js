91ad7439de530fe30350c9613aa40e7fa42fa686
Pedro Guemes <saldunajose@gmail.com>
2023-10-31 06:41:31 -0300

    commit 3

--- app.js
+++ app.js
@@ -75,7 +75,21 @@ function createBoard() {
     square.classList.add('square');
     square.innerHTML = startPiece;
     square.setAttribute('square-id', i);
-    square.classList.add('beige');
+    // square.classList.add('beige');
+    const row = Math.floor((63 - i) / 8) + 1;
+    if (row % 2 === 0) {
+      square.classList.add(i % 2 === 0 ? 'beige' : 'brown');
+    } else {
+      square.classList.add(i % 2 === 0 ? 'brown' : 'beige');
+    }
+
+    if (i <= 15) {
+      square.firstChild.firstChild.classList.add('black');
+    }
+
+    if (i >= 48) {
+      square.firstChild.firstChild.classList.add('white');
+    }
     gameBoard.append(square);
   });
 }
